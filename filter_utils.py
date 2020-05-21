110e7f705a0fedba8465f27c59574925d01ee91c
Alex Spitzer <aes368@cornell.edu>
2020-05-21 17:17:12 -0400

    Add complementary filter and static rpm notch filter

--- filter_utils.py
+++ filter_utils.py
@@ -5,6 +5,7 @@ import joblib
 import numpy as np
 
 from scipy.interpolate import interp1d
+from scipy.signal import lfilter
 
 cachedir = os.path.join(os.path.expanduser('~'), '.cache', 'python_utils')
 memory = joblib.Memory(cachedir, verbose=0)
@@ -33,15 +34,26 @@ class DF1:
 
 @memory.cache
 def exp_smooth(vals, alpha):
-  """ TODO Use a scipy method? """
-  smooth = np.array(vals[0, :], dtype=float)
+  """ y_{t+1} = (1 - alpha) y_t + alpha x_t
+      TODO Use a scipy method? """
+  smooth = np.array(vals[0], dtype=float)
   smoothed = [smooth.copy()]
 
-  for i in range(1, vals.shape[0]):
-    smooth += -alpha * (smooth - vals[i, :])
+  for i in range(1, len(vals)):
+    smooth += -alpha * (smooth - vals[i])
     smoothed.append(smooth.copy())
 
   return np.array(smoothed)
+
+def biquad_notch(freq, fs, Q):
+  om = 2 * np.pi * freq / fs
+  beta = np.tan(om / (2 * Q))
+
+  n1 = 1 / (1 + beta)
+  n2 = -2 * np.cos(om) / (1 + beta)
+  n3 = (1 - beta) / (1 + beta)
+
+  return [n1, n2, n1], [1, n2, n3]
 
 @memory.cache
 def dynamic_rpm_notch(times, rpmtimes, vals, rpms, fs, Q=5.0):
@@ -53,15 +65,7 @@ def dynamic_rpm_notch(times, rpmtimes, vals, rpms, fs, Q=5.0):
 
     freq = rpmnow / 60.0
     if freq < fs / 2.0:
-      om = 2 * np.pi * freq / fs
-      beta = np.tan(om / (2 * Q))
-
-      n1 = 1 / (1 + beta)
-      n2 = -2 * np.cos(om) / (1 + beta)
-      n3 = (1 - beta) / (1 + beta)
-
-      filt.b = [n1, n2, n1]
-      filt.a = [1, n2, n3]
+      filt.b, filt.a = biquad_notch(freq, fs, Q)
 
     else:
       print("WARNING: RPM too high for notch filtering! %d" % rpmnow)
@@ -69,3 +73,34 @@ def dynamic_rpm_notch(times, rpmtimes, vals, rpms, fs, Q=5.0):
     res.append(filt.filter(val))
 
   return np.array(res)
+
+@memory.cache
+def static_rpm_notch(vals, rpm, fs, Q=5.0):
+  freq = rpm / 60.0
+  if freq >= fs / 2.0:
+    raise "ERROR: RPM %d too high for notch filtering" % rpm
+
+  return lfilter(*biquad_notch(freq, fs, Q), vals, axis=0)
+
+@memory.cache
+def complementary_filter(weight, accs, gyros, dt, start_g=np.array((0, 0, 9.81))):
+  assert 0 <= weight <= 1
+  assert len(accs) == len(gyros)
+
+  N = len(accs)
+
+  ss = np.empty((N, 3))
+  s = start_g
+
+  for i, (acc, gyro) in enumerate(zip(accs, gyros)):
+    s_gyro = s + np.cross(s, gyro) * dt
+    s_acc = acc / np.linalg.norm(acc)
+    s = weight * s_acc + (1 - weight) * s_gyro
+    s /= np.linalg.norm(s)
+
+    ss[i] = s
+
+  rolls = np.arctan2(ss[:, 1], ss[:, 2])
+  pitches = np.arcsin(-ss[:, 0])
+
+  return pitches, rolls
