3c3b455b5904b22663f62c9e6f7a4abb21ff94db
Alex Spitzer <aes368@cornell.edu>
2020-06-06 14:23:06 -0400

    Properly fit polynomial trajectories of arbitrary duration

--- polyu.py
+++ polyu.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def deriv_fitting_matrix(degree):
+def deriv_fitting_matrix(degree, t_end=1.0):
   """
     Returns A s.t. that the vector x that satisfies
 
@@ -14,8 +14,8 @@ def deriv_fitting_matrix(degree):
 
     p(0) = b_1
     p^(i)(0) = b_{i+1}
-    p(1) = b_{degree / 2}
-    p^(i)(1) = b_{degree / 2 + i + 1}
+    p(t_end) = b_{degree / 2}
+    p^(i)(t_end) = b_{degree / 2 + i + 1}
 
     i.e. the first degree / 2 derivatives of p at 0 match the first degree / 2
          entries of b and the first degree / 2 derivatives of p at 1, match the last
@@ -26,24 +26,17 @@ def deriv_fitting_matrix(degree):
 
   A = np.zeros((degree, degree))
 
+  ts = t_end ** np.array(range(degree))
+
   constant_term = 1
   poly = np.ones(degree)
   for i in range(degree // 2):
     A[i, i] = constant_term
-    A[degree // 2 + i, :] = np.hstack((np.zeros(i), poly))
+    A[degree // 2 + i, :] = np.hstack((np.zeros(i), poly * ts[:degree - i]))
     poly = np.polynomial.polynomial.polyder(poly)
     constant_term *= (i + 1)
 
   return A
-
-def change_poly_duration(poly, duration):
-  new_poly = poly.copy()
-  divider = 1
-  for i in range(len(new_poly)):
-    new_poly[i] = poly[i] / divider
-    divider *= duration
-
-  return new_poly
 
 if __name__ == "__main__":
   for i in range(1, 5):
