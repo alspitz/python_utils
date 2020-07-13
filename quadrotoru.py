811d731f723b1bf605a3f23788a3f8d289eb50cb
Alex Spitzer <aes368@cornell.edu>
2020-07-13 00:37:10 -0400

    quadrotoru: Add function to invert motor model to get RPMs from RFs

--- quadrotoru.py
+++ quadrotoru.py
@@ -8,8 +8,10 @@ class Quadrotor:
      motor_torque_scale: c s.t. torque = c * force(rpm)
      inertia: torque = I * alpha
   """
-  def __init__(self, *, motor_thrust_coeffs, motor_torque_scale, inertia, motor_arm_length, motor_spread_angle, motor_inertia=0.0, center_of_mass=np.zeros(3)):
+  def __init__(self, *, mass, motor_thrust_coeffs, motor_torque_scale, inertia, motor_arm_length, motor_spread_angle, motor_inertia=0.0, center_of_mass=np.zeros(3)):
     self.motor_thrust = np.poly1d(motor_thrust_coeffs)
+
+    self.mass = mass
 
     self.I = inertia
     self.I_inv = np.linalg.inv(self.I)
@@ -27,6 +29,8 @@ class Quadrotor:
     self.mixer[1, :] = np.array((-dsms, dsms, dsms, -dsms))
     self.mixer[2, :] = np.array((-dcms, dcms, -dcms, dcms))
     self.mixer[3, :] = - motor_torque_scale * self.motor_dirs
+
+    self.mixer_inv = np.linalg.inv(self.mixer)
 
     # torque = com x (0, 0, thrust) = [com]_x * (0, 0, thrust)
     self.com_thrust_torque = skew_matrix(center_of_mass)[:, 2][:, np.newaxis]
@@ -54,6 +58,9 @@ class Quadrotor:
     """
     return -self.motor_I.dot(self._zvecfromrpm(rpmsd).T)
 
+  def euler_angvel_term(self, angvel):
+    return np.cross(angvel.T, self.I.dot(angvel.T), axis=0)
+
   def angaccel(self, *, rpms, angvel_in_body, rpmsd=None):
     """ Accepts multiple data points as rows; returns as rows. """
     if rpmsd is None:
@@ -69,8 +76,28 @@ class Quadrotor:
 
     com_torque = self.com_thrust_torque * thrust
 
-    total_torque = torque - np.cross(angvel_in_body.T, self.I.dot(angvel_in_body.T), axis=0) - com_torque + extra_torque
+    total_torque = torque - self.euler_angvel_term(angvel_in_body) - com_torque + extra_torque
 
     angacc = self.I_inv.dot(total_torque)
 
     return angacc
+
+  def rotorforces_from_accels(self, zaccel, angaccel, angvel_in_body=None):
+    """ NOTE: Does not consider com, rotor accel torque, or gyro torque """
+    thrust = self.mass * zaccel
+    torque = self.I.dot(angaccel.T).T
+    if angvel_in_body is not None:
+      torque += self.euler_angvel_term(angvel_in_body).T
+
+    wrench = np.hstack((thrust[:, np.newaxis], torque))
+
+    return self.mixer_inv.dot(wrench.T).T
+
+  def rpms_from_rotorforces(self, rf):
+    flatrf = rf.flatten()
+    flatrpm = np.array([np.max((self.motor_thrust - f).roots) for f in flatrf])
+    return flatrpm.reshape(rf.shape)
+
+  def rpms_from_accels(self, zaccel, angaccel, angvel_in_body=None):
+    rotorforces = self.rotorforces_from_accels(zaccel, angaccel, angvel_in_body)
+    return self.rpms_from_rotorforces(rotorforces)
