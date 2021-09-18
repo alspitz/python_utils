de198af166c83fe0dba08c184a2ff6630e6253f7
Alex Spitzer <aes368@cornell.edu>
2021-09-18 15:55:32 -0400

    rigid_body: Separate out attitude dynamics into Attitude class

--- rigid_body.py
+++ rigid_body.py
@@ -1,6 +1,41 @@
 import numpy as np
 
-from python_utils.mathu import quat_identity, quat_mult, vector_quat
+from python_utils.mathu import quat_identity, quat_mult, vector_quat, matrix_from_quat
+
+from scipy.spatial.transform import Rotation as R
+
+class Attitude(object):
+  def __init__(self, quat=quat_identity(), ang=np.zeros(3), in_body=False):
+    self.quat = quat.copy()
+    self.ang = ang.copy()
+    self.in_body = in_body
+
+  def step(self, dt, accel=np.zeros(3), ang_accel=np.zeros(3)):
+    if self.in_body:
+      quat_deriv = quat_mult(self.quat, vector_quat(self.ang)) / 2.0
+      quat_dd = quat_mult(self.quat, vector_quat(ang_accel)) / 2.0
+    else:
+      quat_deriv = quat_mult(vector_quat(self.ang), self.quat) / 2.0
+      # TODO Is this right?
+      quat_dd = quat_mult(vector_quat(ang_accel), self.quat) / 2.0
+
+    self.quat += quat_deriv * dt + 0.5 * quat_dd * dt ** 2
+    self.ang += ang_accel * dt
+
+    self.quat /= np.linalg.norm(self.quat)
+
+  def get_quat(self):
+    return self.quat.copy()
+
+  def get_rot(self):
+    return matrix_from_quat(self.quat)
+
+  def get_ang(self):
+    return self.ang.copy()
+
+  def set_rot(self, rot):
+    quat = R.from_matrix(rot).as_quat()
+    self.quat = np.array((quat[3], quat[0], quat[1], quat[2]))
 
 class RigidBody3D(object):
   """
@@ -8,19 +43,26 @@ class RigidBody3D(object):
     ang and ang_accel are in the world frame and radians.
     vel and accel are in the world frame and meters.
   """
-  def __init__(self, pos=np.zeros(3), vel=np.zeros(3), quat=quat_identity(), ang=np.zeros(3)):
+  def __init__(self, pos=np.zeros(3), vel=np.zeros(3), quat=quat_identity(), ang=np.zeros(3), in_body=False):
     self.pos = pos.copy()
     self.vel = vel.copy()
     self.quat = quat.copy()
     self.ang = ang.copy()
+    self.in_body = in_body
 
   def step(self, dt, accel=np.zeros(3), ang_accel=np.zeros(3)):
     self.pos += self.vel * dt + 0.5 * accel * dt ** 2
     self.vel += accel * dt
 
-    quat_deriv = quat_mult(vector_quat(self.ang), self.quat) / 2.0
-    # TODO Use ang accel to update quat as for position above.
-    self.quat += quat_deriv * dt
+    if self.in_body:
+      quat_deriv = quat_mult(self.quat, vector_quat(self.ang)) / 2.0
+      quat_dd = quat_mult(self.quat, vector_quat(ang_accel)) / 2.0
+    else:
+      quat_deriv = quat_mult(vector_quat(self.ang), self.quat) / 2.0
+      # TODO Is this right?
+      quat_dd = quat_mult(vector_quat(ang_accel), self.quat) / 2.0
+
+    self.quat += quat_deriv * dt + 0.5 * quat_dd * dt ** 2
     self.ang += ang_accel * dt
 
     self.quat /= np.linalg.norm(self.quat)
@@ -34,8 +76,15 @@ class RigidBody3D(object):
   def get_quat(self):
     return self.quat.copy()
 
+  def get_rot(self):
+    return matrix_from_quat(self.quat)
+
   def get_ang(self):
     return self.ang.copy()
+
+  def set_rot(self, rot):
+    quat = R.from_matrix(rot).as_quat()
+    self.quat = np.array((quat[3], quat[0], quat[1], quat[2]))
 
 if __name__ == "__main__":
   body = RigidBody3D()
