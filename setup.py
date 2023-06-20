8a87673894f43cef800f4df706094492251104fe
Jakub Filipek <jakub@overland.ai>
2023-06-20 15:28:36 -0700

    fix missing comma

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="python_utils",
-    package_dir={"": "python_utils"}
+    package_dir={"": "python_utils"},
     author="Alex Spitzer",
     license=open("LICENSE", mode="r").read(),
 )
