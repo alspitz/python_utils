635a505a3dc59f1f647d7419d77c9d33153285d7
Alexander Spitzer <aes368@cornell.edu>
2023-06-20 16:15:29 -0700

    Udpate setup.py to call find_packages
    Also add vim swp to gitignore

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="python_utils",
+    packages=find_packages(),
     author="Alex Spitzer",
     license=open("LICENSE", mode="r").read(),
 )
