643b458248f4fdec5c111687dfa929498f28f7e6
Alexander Spitzer <aes368@cornell.edu>
2024-03-09 12:30:10 -0800

    Add more info to setup.py, including version

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="python_utils",
+    version="0.1.0",
     packages=find_packages(),
     author="Alex Spitzer",
+    maintainer="Alex Spitzer",
+    url="https://github.com/alspitz/python_utils",
     license=open("LICENSE", mode="r").read(),
     install_requires=(
       'numpy',
